935b9a6984d1dd244eae62dad6b11b67ea28a616
bblinder <filmmaven@gmail.com>
2019-11-18 14:09:18 -0500

    modified:   build.sh

--- blindy_ubuntu/build.sh
+++ blindy_ubuntu/build.sh
@@ -6,3 +6,5 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd $ROOT_DIR
 
 docker build -t blindy_ubuntu -f blindy_ubuntu/Dockerfile .
+
+## when done, run this: "docker run -d -it --name blindy_ubuntu blindy_ubuntu"
